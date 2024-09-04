7313f176157e6f9f30e59a82250b081a06e2479a
Rinwaoluwa <117735533+Rinwaoluwa@users.noreply.github.com>
2024-09-04 17:33:14 +0100

    Add global state to cart summary.

--- src/components/ui/checkout/cart-summary-table.tsx
+++ src/components/ui/checkout/cart-summary-table.tsx
@@ -11,11 +11,18 @@ import {
   TableRow,
 } from "@/components/ui/shadcn/table";
 import { YnsLink } from "@/components/yns-link";
-import { products } from "@/lib/constants";
+import { useAppSelector } from "@/lib/redux/hooks";
+import { RootState } from "@/lib/redux/store";
+import { FormEvent } from "react";
 
 export const CartSummaryTable = ({ cart }: { cart: any }) => {
+  const products = useAppSelector((state: RootState) => state.cart);
+  const cartTotal = products.reduce(
+    (acc, sum) => acc + sum.price * sum.quantity,
+    0
+  );
   return (
-    <form className="max-w-2xl">
+    <form className="max-w-2xl" onSubmit={(e: FormEvent) => e.preventDefault()}>
       <h1 className="text-3xl font-bold mb-6">Your cart</h1>
       <Table>
         <TableHeader>
@@ -74,48 +81,24 @@ export const CartSummaryTable = ({ cart }: { cart: any }) => {
                     </button>
                   </span>
                 </TableCell>
-                <TableCell className="text-right">₦{/*Total*/}6</TableCell>
+                <TableCell className="text-right">₦{cartTotal}</TableCell>
               </TableRow>
             );
           })}
-          {cart.shippingRate && (
-            <TableRow>
-              <TableCell className="hidden sm:table-cell sm:w-24"></TableCell>
-              <TableCell className="font-medium" colSpan={3}>
-                {/* Delivery estimate */}
-                {cart.shippingRate.display_name} 45 days
-              </TableCell>
-              <TableCell className="text-right">
-                {/* shopping rate */}
-                45
-              </TableCell>
-            </TableRow>
-          )}
+          <TableRow>
+            <TableCell className="hidden sm:table-cell sm:w-24"></TableCell>
+            <TableCell className="font-medium" colSpan={3}>
+              {/* Delivery estimate */}
+              Delivery Estimate: {products.length > 3 ? "3 days" : "1 day"}
+            </TableCell>
+            <TableCell className="text-right">
+              {/* shopping rate */}
+              {Math.ceil((cartTotal / 100) * 2)}%
+            </TableCell>
+          </TableRow>
+
         </TableBody>
       </Table>
     </form>
-    /*<span class="flex flex-row items-center text-foreground"><button class="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md text-xs group aspect-square p-0" type="submit" formaction="javascript:throw new Error('A React form was unexpectedly submitted. If you called form.submit() manually, consider using form.requestSubmit() instead. If you\'re trying to use event.stopPropagation() in a submit event handler, consider also calling event.preventDefault().')"><span class="flex h-4 w-4 items-center justify-center rounded-full bg-neutral-100 pb-0.5 font-bold leading-none text-black transition-colors group-hover:bg-neutral-500 group-hover:text-white">–</span></button><span class="inline-block min-w-8 px-1 text-center tabular-nums">1</span><button class="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md text-xs group aspect-square p-0" type="submit" formaction="javascript:throw new Error('A React form was unexpectedly submitted. If you called form.submit() manually, consider using form.requestSubmit() instead. If you\'re trying to use event.stopPropagation() in a submit event handler, consider also calling event.preventDefault().')"><span class="flex h-4 w-4 items-center justify-center rounded-full bg-neutral-100 pb-0.5 font-bold leading-none text-black transition-colors group-hover:bg-neutral-500 group-hover:text-white">+</span></button></span>*/
-    // <TableFooter>
-    //   {cart.taxBreakdown.map((tax, idx) => (
-    //     <TableRow key={idx + tax.taxAmount} className="font-normal">
-    //       <TableCell className="hidden w-24 sm:table-cell"></TableCell>
-    //       <TableCell colSpan={3} className="text-right">
-    //         {tax.taxType.toLocaleUpperCase()} {tax.taxPercentage}%
-    //       </TableCell>
-    //       <TableCell className="text-right">
-    //         {/* <CartAmountWithSpinner total={tax.taxAmount} currency={currency} /> */}
-    //       </TableCell>
-    //     </TableRow>
-    //   ))}
-    //   <TableRow className="text-lg font-bold">
-    //     <TableCell className="hidden w-24 sm:table-cell"></TableCell>
-    //     <TableCell colSpan={3} className="text-right">
-    //       Total Summary
-    //     </TableCell>
-    //     <TableCell className="text-right">
-    //       {/* <CartAmountWithSpinner total={total} currency={currency} /> */}
-    //     </TableCell>
-    //   </TableRow>
-    // </TableFooter>
   );
 };
